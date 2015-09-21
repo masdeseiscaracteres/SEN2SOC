384b2286a1946e0f79127351c10ad3808383abdd
Óscar González <masdeseiscaracteres@gmail.com>
2015-09-21 22:16:25 +0200

    Add some usage examples

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
 # import <package.module>
 import json
+import datetime
+
 # from <package.module> import <class> as <alias>
 from SEN2SOC import SEN2SOCApi
 
-response_list=SEN2SOCApi.getlastnodevalues(4)
+###################################################################
+# This file contains usage examples for the SEN2SOC Python module #
+###################################################################
+
+########################### Static sensors ########################
+
+# Get list of static sensors
+response_list=SEN2SOCApi.getnodes()
+print(json.dumps(response_list, sort_keys=True, indent=4))
+
+# Get last values for environmental sensors
+response_list=SEN2SOCApi.getlastenvironmentalvalues()
+print(json.dumps(response_list, sort_keys=True, indent=4))
+
+# Get last values for irrigation sensors
+response_list=SEN2SOCApi.getlastirrigationvalues()
+print(json.dumps(response_list, sort_keys=True, indent=4))
+
+############################ Mobile sensors #######################
+
+# Get list of mobile sensors
+response_list=SEN2SOCApi.getmobilenodes()
+print(json.dumps(response_list, sort_keys=True, indent=4))
+
+# Get last values for mobile sensors
+response_list=SEN2SOCApi.getlastmobilevalues()
+print(json.dumps(response_list, sort_keys=True, indent=4))
+
+################# Both static & mobile sensors #####################
+
+# Get information of a specific sensor
+response_list=SEN2SOCApi.getlastnodevalues(3180)
+print(json.dumps(response_list, sort_keys=True, indent=4))
+
+# Get historic values for the specified date interval
+enddate = datetime.datetime.today()
+startdate = enddate-datetime.timedelta(days=2)
+response_list=SEN2SOCApi.gethistoricnodevalues(3180,startdate,enddate)
 print(json.dumps(response_list, sort_keys=True, indent=4))
